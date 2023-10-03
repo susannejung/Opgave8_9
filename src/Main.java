d4bfc6fe4a19fc12c80b6b5943c7a6f775285592
SUHA <suha@zealand.dk>
2023-10-03 14:40:59 +0200

    Version2

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,31 @@ public class Main {
                 board[i][j]=' ';
 
         Scanner input=new Scanner(System.in);
-        System.out.println("Spiller 1 angiv række og kolonne");
-        int r,k;
-        r=input.nextInt();
-        k=input.nextInt();
-        board[r][k]='X';
 
-
-        udskrivBoard(board);
+        int r,k,spillerNr=1;
+        boolean vundet=false;
+        while(vundet==false) {
+            System.out.println("Spiller "+ spillerNr+" angiv række og kolonne");
+            r = input.nextInt();
+            k = input.nextInt();
+            if(spillerNr==1)
+                board[r][k] = 'X';
+            else
+                board[r][k] = 'O';
+            udskrivBoard(board);
+            if(checkFunderVinder(board)==1) {
+                System.out.println("Spiller 1 har vundet");
+                vundet = true;
+            }
+            if(checkFunderVinder(board)==2) {
+                System.out.println("Spiller 2 har vundet");
+                vundet = true;
+            }
+            if(spillerNr==1)
+                spillerNr=2;
+            else
+                spillerNr=1;
+        }
     }
 
     public static void udskrivBoard(char [][] a){
@@ -27,5 +44,26 @@ public class Main {
         System.out.printf("_________________\n");
         System.out.printf("| %c  |  %c  | %c  |\n",a[2][0],a[2][1],a[2][2]);
         System.out.printf("_________________\n");
+    }
+
+
+    public static int checkFunderVinder(char [][] a){
+        int antalX=0;
+        int antalO=0;
+        for(int i=0;i<3;i++) {
+            antalX = 0;
+            antalO = 0;
+            for (int j = 0; j < 3; j++) {
+                if (a[i][j] == 'X')
+                    antalX++;
+                if(a[i][j]=='O')
+                    antalO++;
+            }
+            if(antalX==3)
+                return 1;
+            if(antalO==3)
+                return 2;
+        }
+        return 0;
     }
 }
