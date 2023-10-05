2514b33ab79191e1bc18f03703b776ce3d40801a
SUHA <suha@zealand.dk>
2023-10-05 14:23:36 +0200

    Version3

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,24 @@ public class Main {
                 board[i][j]=' ';
 
         Scanner input=new Scanner(System.in);
-
+        int antalInput=0;
         int r,k,spillerNr=1;
         boolean vundet=false;
-        while(vundet==false) {
+        while(vundet==false && antalInput<9) {
             System.out.println("Spiller "+ spillerNr+" angiv række og kolonne");
             r = input.nextInt();
             k = input.nextInt();
+            antalInput++;
             if(spillerNr==1)
                 board[r][k] = 'X';
             else
                 board[r][k] = 'O';
             udskrivBoard(board);
-            if(checkFunderVinder(board)==1) {
+            if(checkFundetVinder(board)==1) {
                 System.out.println("Spiller 1 har vundet");
                 vundet = true;
             }
-            if(checkFunderVinder(board)==2) {
+            if(checkFundetVinder(board)==2) {
                 System.out.println("Spiller 2 har vundet");
                 vundet = true;
             }
@@ -37,17 +38,23 @@ public class Main {
     }
 
     public static void udskrivBoard(char [][] a){
-        System.out.printf("_________________\n");
+       /* System.out.printf("_________________\n");
         System.out.printf("| %c  |  %c  | %c  |\n",a[0][0],a[0][1],a[0][2]);
         System.out.printf("_________________\n");
         System.out.printf("| %c  |  %c  | %c  |\n",a[1][0],a[1][1],a[1][2]);
         System.out.printf("_________________\n");
         System.out.printf("| %c  |  %c  | %c  |\n",a[2][0],a[2][1],a[2][2]);
+        System.out.printf("_________________\n");*/
+
+        for(int i=0;i<3;i++){
+            System.out.printf("_________________\n");
+            System.out.printf("| %c  |  %c  | %c  |\n",a[i][0],a[i][1],a[i][2]);
+        }
         System.out.printf("_________________\n");
     }
 
 
-    public static int checkFunderVinder(char [][] a){
+    public static int checkFundetVinder(char [][] a){
         int antalX=0;
         int antalO=0;
         for(int i=0;i<3;i++) {
@@ -64,6 +71,33 @@ public class Main {
             if(antalO==3)
                 return 2;
         }
+
+        for(int k=0;k<3;k++){
+            antalX = 0;
+            antalO = 0;
+            for(int r=0;r<3;r++){
+                if (a[r][k] == 'X')
+                    antalX++;
+                if(a[r][k]=='O')
+                    antalO++;
+            }
+            if(antalX==3)
+                return 1;
+            if(antalO==3)
+                return 2;
+        }
+
+        if(a[0][0]=='X' && a[1][1]=='X' && a[2][2]=='X')
+            return 1;
+        if(a[0][2]=='X' && a[1][1]=='X' && a[2][0]=='X')
+            return 1;
+        if(a[0][0]=='0' && a[1][1]=='0' && a[2][2]=='0')
+            return 2;
+        if(a[0][2]=='0' && a[1][1]=='0' && a[2][0]=='0')
+            return 2;
+
+
+
         return 0;
     }
 }
